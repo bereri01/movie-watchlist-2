da5f0820cd2890f7cd793a5966ceff60e149548c
bereri01 <bereri01@students.zhaw.ch>
2026-04-03 15:49:03 +0200

    commit

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -23,9 +23,7 @@
     <div class="collapse navbar-collapse" id="navbarNav">
       <ul class="navbar-nav">
         <li class="nav-item">
-          <a class="nav-link active" aria-current="page" href="/movies"
-            >Movies</a
-          >
+          <a class="nav-link active" aria-current="page" href="/movies">Movies!</a>
         </li>
       </ul>
     </div>
